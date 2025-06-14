1f1d63c3d9f1006f70a92c8c27c3db0b1296dfbe
shalinth-adith <shalinth.adith@outlook.com>
2025-06-14 12:49:49 +0530

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by shalinth adithyan on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EdutainmentTests/EdutainmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentTests.swift
+//  EdutainmentTests
+//
+//  Created by shalinth adithyan on 14/06/25.
+//
+
+import Testing
+@testable import Edutainment
+
+struct EdutainmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
