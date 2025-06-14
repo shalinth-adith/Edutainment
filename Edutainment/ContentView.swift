2e68d4b3fe7111ac2dfc3e4f6b7e74c14b1a53b9
shalinth-adith <shalinth.adith@outlook.com>
2025-06-14 14:46:41 +0530

    basic layout

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -4,18 +4,42 @@
 //
 //  Created by shalinth adithyan on 14/06/25.
 //
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+struct ContentView : View {
+    enum GameState {
+        case settings
+        case playing
+        case result
+    }
+    @State private var gameState : GameState = .settings
+    @State private var uptoTable = 5
+    @State private var numberOfQuestions : Int = 10
+    
+    var body : some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Multiplication Game ")
+                .font(.largeTitle)
+                .bold(true)
+            Stepper("Practice table upto : \(uptoTable)" , value:$uptoTable ,in:2...20 )
+            
+            Picker("Number of questions ", selection: $numberOfQuestions){
+                Text("5").tag(5)
+                Text("10").tag(10)
+                Text("15").tag(15)
+                Text("20").tag(20)
+            }
+            .pickerStyle(.segmented)
+            
+            Button("Start"){
+                startGame()
+                
+            }
         }
-        .padding()
+    
+    }
+    func startGame(){
+        
     }
 }
 
